3567efd58ee524ed3f40121d061e7f266287cd0e
Vasil Trifonov <v.trifonov@gmail.com>
2018-09-21 15:14:44 +0300

    Fix retries in the messaging service Do not poll if tab is inactive

--- src/services/helpers-service.ts
+++ src/services/helpers-service.ts
@@ -5,16 +5,44 @@ declare function escape(s: string): string;
 declare function unescape(s: string): string;
 
 export class HelpersService {
+    private getHiddenProperty(): string {
+        var prefixes = ['webkit', 'moz', 'ms', 'o'];
+        if ('hidden' in document) {
+            return 'hidden';
+        }
+        for (var i = 0; i < prefixes.length; i++) {
+            if ((prefixes[i] + 'Hidden') in document)
+                return prefixes[i] + 'Hidden';
+        }
+        return null;
+    }
+
+    isBrowserTabActive() {
+        if(!this.isBrowser()) {
+            // not in browser
+            return true;
+        }
+
+        var property = this.getHiddenProperty();
+        if (!property) return true;
+
+        return !document[property];
+    }
+
 	areCaseInsensitiveEqual(value1: string, value2: string): boolean {
 		return (value1 || "").toLowerCase() == (value2 || "").toLowerCase();
 	}
 
 	shortId(): string {
 		return shortId.generate();
-	}
+    }
+
+    isBrowser(): boolean {
+        return (typeof window !== "undefined");
+    }
 
 	base64Encode(input: string): string {
-		if (typeof window !== "undefined") {
+		if (this.isBrowser()) {
 			return window.btoa(unescape(encodeURIComponent(input)))
 		}
 		else {
